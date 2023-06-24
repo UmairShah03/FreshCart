724662f7e9883d4953d2443d4b472f080fab392d
Abrar sheikh <abrarsheikhkhan5500@gmail.com>
2023-06-24 23:21:25 +0500

    Committing local changes before merge

--- dashboard/add-parent-category.php
+++ dashboard/add-parent-category.php
@@ -2,3 +2,28 @@
 include "header.php";
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=\, initial-scale=1.0">
+    <title>add parent category</title>
+    <style></style>
+</head>
+<body>
+    
+<div class="container">
+        <form action=""method= "post" >
+
+            <input type="text" name="name" id="">
+
+
+            <input type="file" name="image" id="">
+
+            <input type="submit" value="Submit" name ="btn">
+        </form>
+    </div>
+</body>
+</html>
